c291851dc355adab2bb166394f26b76d267b06e5
Terry Thompson <terrylthompsonintx@gmail.com>
2020-08-04 17:12:54 -0500

    Updates

---
+++ precache-manifest.69139259c6cc71afd2196868d0b78ba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef3dd556e84a5a457e336e4f8eecfac",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "473f6944bdbbbe4a61b0",
+    "url": "/react-portfolio/static/js/2.c1d0bceb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.c1d0bceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2014f56ed0b3bac58c4e",
+    "url": "/react-portfolio/static/js/main.053cb75a.chunk.js"
+  },
+  {
+    "revision": "9ceac9bea1993d11a44b",
+    "url": "/react-portfolio/static/js/runtime-main.4a860f54.js"
+  }
+]);
